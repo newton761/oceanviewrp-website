8825b15725c3c7cc479d11ba06b9fc80dd98c173
Isaac Kwame <plange761@gmail.com>
2026-07-28 16:33:49 -0600

    Fix event import

--- app/events/events.ts
+++ app/events/events.ts
@@ -1,11 +1,11 @@
 const event = {
-  title: "Hide & Steal Challenge",
-  date: "July 24, 2026",
+  title: "Black Motors is opened",
+  date: "July 25, 2026",
   time: "10:00 PM GMT",
   location: "Los Santos",
-  flyer: "/events/car-hunt.jpg",
+  flyer: "/events/black.jpg",
   description:
-    "Find the hidden one-of-one custom vehicle before anyone else.",
+    "New dealership opening! Come check out the new Black Motors dealership in Los Santos! We have a wide selection of vehicles available for purchase, as well as a team of knowledgeable staff to help you find the perfect car for your needs. Don't miss out on this exciting event!",
 };
 
 export default event;
