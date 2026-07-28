744f83ecc25b831e80076730df42621914658c32
Isaac Kwame <plange761@gmail.com>
2026-07-28 15:33:24 -0600

    Add events page

---
+++ app/events/page.tsx
@@ -0,0 +1,18 @@
+export const events = [
+  {
+    title: "One of One Vehicle Hunt",
+    date: "July 30, 2026",
+    location: "Legion Square",
+    flyer: "/events/car-hunt.jpg",
+    description:
+      "Find the hidden one-of-one vehicle before anyone else.",
+  },
+  {
+    title: "EMS Recruitment",
+    date: "August 5, 2026",
+    location: "Pillbox Hospital",
+    flyer: "/events/ems.jpg",
+    description:
+      "Join EMS and begin your medical career in OceanViewRP.",
+  },
+];
