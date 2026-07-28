27a8b88aaac1ff06567cd02f628d1d492c46bb06
Isaac Kwame <plange761@gmail.com>
2026-07-28 16:21:38 -0600

    Add current event data

--- app/events/events.ts
+++ app/events/events.ts
@@ -2,10 +2,10 @@ const event = {
   title: "Hide & Steal Challenge",
   date: "July 24, 2026",
   time: "10:00 PM GMT",
-  location: "Somewhere in Los Santos",
+  location: "Los Santos",
   flyer: "/events/car-hunt.jpg",
   description:
-    "Find the hidden one-of-one custom vehicle before anyone else. If you escape with it, it's yours forever.",
+    "Find the hidden one-of-one custom vehicle before anyone else.",
 };
 
 export default event;
